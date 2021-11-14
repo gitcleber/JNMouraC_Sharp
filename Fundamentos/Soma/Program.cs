1f64df16b99d3c235913b33281bee84ee5b92bba
gitcleber <cleberhill@gmail.com>
2021-11-14 20:28:56 -0300

    - Created a program to sum all the numbers of an example range (0 to 10 or 10 to 100).

---
+++ Fundamentos/Soma/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Soma
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int soma = 0;
+            for (int i = 1; i < 5; i++)
+            {
+                /*Faça um programa para somar todos os numeros de um range*/
+
+                Console.WriteLine("Digite o {0}º numero: ",i);
+                soma += int.Parse(Console.ReadLine());
+            }
+            Console.WriteLine("Soma Total: " + soma);
+            Console.ReadLine();
+        }
+    }
+}
