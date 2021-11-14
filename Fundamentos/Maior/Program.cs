3207d3e0eb64005c1dab7dd4e0690f82acd3fb23
gitcleber <cleberhill@gmail.com>
2021-11-14 19:01:39 -0300

    - add a 'idxNum' variable for save the value of 'i' when change the value of 'maior' var; - update the message for output a value of idxNum var.

--- Fundamentos/Maior/Program.cs
+++ Fundamentos/Maior/Program.cs
@@ -8,15 +8,18 @@ namespace Maior
 
         static void Main(string[] args)
         {
-            int maior = int.MinValue, num;
+            int maior = int.MinValue, num, idxNum=0;
             for (int i = 1; i < 6; i++)
             {
                 Console.Write("Digite o " + i + "º numero: ");
                 num = int.Parse(Console.ReadLine());
                 if (maior < num)
+                {
                     maior = num;
+                    idxNum = i;
+                }
             }
-            Console.WriteLine("Maior = " + maior);
+            Console.WriteLine("Maior Número é o {0} digitado na {1}ª rodada",maior, idxNum);
 
             Console.ReadLine();
 
