c0e8114a7d721e9349525f6b21ddb7aba5a3be92
gitcleber <cleberhill@gmail.com>
2021-11-14 18:52:09 -0300

    - Created a project named 'Maior' for practice 'else/if' conditional statement and loop 'for'.

--- Fundamentos/Maior/Program.cs
+++ Fundamentos/Maior/Program.cs
@@ -4,9 +4,22 @@ namespace Maior
 {
     class Program
     {
+        /*Faça um programa que leia 5 números inteiros informados pelo usuário e, no final, mostre o maior deles.*/
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int maior = int.MinValue, num;
+            for (int i = 1; i < 6; i++)
+            {
+                Console.Write("Digite o " + i + "º numero: ");
+                num = int.Parse(Console.ReadLine());
+                if (maior < num)
+                    maior = num;
+            }
+            Console.WriteLine("Maior = " + maior);
+
+            Console.ReadLine();
+
         }
     }
 }
