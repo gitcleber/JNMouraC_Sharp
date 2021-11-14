4cda12ac97883523bcbb0e3c09f7d4cda9c7178f
gitcleber <cleberhill@gmail.com>
2021-11-14 20:16:41 -0300

    - Create a program that prints the even numbers of an example range (0 to 10 or 10 to 100).

---
+++ Fundamentos/Pares/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Pares
+{
+    class Program
+    {
+        /* Faça um programa que imprime os numeros pares de um range exemplo(0 a 10 ou 10 a 100)*/
+        static void Main(string[] args)
+        {
+            /*
+            for (int i = 10; i < 101; i++)
+            {
+                if (i % 2 == 0)
+                    Console.WriteLine(i);
+            }
+            */
+            for (int i = 10; i < 101; i += 2)
+                Console.WriteLine(i);
+        }
+    }
+}
