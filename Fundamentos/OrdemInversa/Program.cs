092e518510747a20f21bae09c17aa608be031510
gitcleber <cleberhill@gmail.com>
2021-11-18 09:28:43 -0300

    - Create a algoritm for practice concepts of Stack, Queue with Arrays.

---
+++ Fundamentos/OrdemInversa/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace OrdemInversa
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Criando o Array
+            Console.WriteLine("Gerando array de números");            
+            int[] numeros = { 16, 15, 3, 4, 8, 11, 23 };
+
+            Console.WriteLine("Array");
+            for (int i = 0; i < numeros.Length; i++)
+            {                
+                Console.WriteLine(numeros[i]);
+            }
+
+            //Criando
+            Stack<int> pilha = new Stack<int>();
+            for (int i = 0; i < numeros.Length; i++)
+            {
+                pilha.Push(numeros[i]);
+            }
+
+            Console.WriteLine("Pilha");
+            foreach (var item in pilha)
+            {                
+                Console.WriteLine(item);
+            }
+
+            Queue<int> fila = new Queue<int>();
+            for (int i = 0; i < pilha.Count; i++)
+            {
+                fila.Enqueue(pilha.Pop());
+            }
+            Console.WriteLine("Fila");
+            for (int i = 0; i < fila.Count; i++)
+            {                
+                Console.WriteLine(fila.Dequeue());
+            }
+        }
+    }
+}
