d8976c5a95a287bda9da82b99b32fccb5a4208bb
gitcleber <cleberhill@gmail.com>
2021-11-18 14:15:15 -0300

    - Created a Project named 'Porta' for practice OO in C#.

---
+++ OO/Porta/Porta.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Porta
+{/*
+          * * --exe02--
+          * 
+           Crie uma classe que atenda aos requisitos abaixo:
+            Classe: Porta
+            Atributos: aberta, cor
+            Métodos: void abrir(), void fechar(), void pintar(string cor)
+            Para testar a classe, crie um objeto porta, abra e feche a mesma e pinte‐a de diversas cores.
+            Importante: Defina os tipos dos atributos conforme sua necessidade e lembre-se: uma porta que
+            já está aberta não pode abrir novamente, como também, uma porta que já está fechada não pode
+            ser fechada novamente.
+            Projeto Porta
+          * */
+    class Porta
+    {
+        public bool Aberta { get; set; }
+        public string Cor { get; set; }
+
+        public void Abrir()
+        {
+            if(!Aberta)
+                this.Aberta = true;
+            else
+                Console.WriteLine("A porta já está Aberta.");
+        }
+        public void Fechar()
+        {
+            if(Aberta)
+                this.Aberta = false;
+            else
+                Console.WriteLine("A porta já está fechada.");
+        }
+        public void Pintar(string cor)
+        {
+            this.Cor = cor;
+        }
+    }
+}
---
+++ OO/Porta/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace Porta
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {/*
+          * * --exe02--
+          * 
+           Crie uma classe que atenda aos requisitos abaixo:
+            Classe: Porta
+            Atributos: aberta, cor
+            Métodos: void abrir(), void fechar(), void pintar(string cor)
+            Para testar a classe, crie um objeto porta, abra e feche a mesma e pinte‐a de diversas cores.
+            Importante: Defina os tipos dos atributos conforme sua necessidade e lembre-se: uma porta que
+            já está aberta não pode abrir novamente, como também, uma porta que já está fechada não pode
+            ser fechada novamente.
+            Projeto Porta
+          * */
+            Console.WriteLine("Hello World!");
+            Porta porta1 = new Porta();
+            Console.WriteLine($"Porta está Aberta?: {porta1.Aberta}");            
+            porta1.Abrir();
+            porta1.Abrir();
+            Console.WriteLine($"A porta esta Aberta?: {porta1.Aberta}");
+            porta1.Fechar();
+            Console.WriteLine($"A porta está Aberta?: {porta1.Aberta}");
+
+            porta1.Pintar("azul");
+            Console.WriteLine($"A porta está na cor: {porta1.Cor}");
+
+
+
+            Console.ReadLine();
+
+        }
+    }
+}
