d068a34cf309cfd2a555b5702d3775be342289e2
gitcleber <cleberhill@gmail.com>
2021-11-18 14:07:48 -0300

    - Created a Project named 'Jogador' for practice OO in C#.

---
+++ OO/Jogador/Jogador.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Jogador
+{
+    class Jogador
+    {
+        public string Nome { get; set; }
+        public string RG { get; set; }
+        public string CPF { get; set; }
+        public string Endereco { get; set; }
+
+        public Jogador()
+        {
+            this.Nome = "";
+            this.RG = "";
+            this.Endereco = "";
+            this.CPF = "";
+        }
+    }
+}
---
+++ OO/Jogador/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Jogador
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Jogador zequinha = new Jogador();
+            zequinha.CPF = "29368591855";
+            zequinha.Nome = "Zeca";
+            zequinha.Endereco = "Rua 1";
+            zequinha.RG = "1321654x";
+
+            Jogador mariazinha = new Jogador();
+            mariazinha.CPF = "29368591856";
+            mariazinha.Nome = "Maria";
+            mariazinha.Endereco = "Rua 1";
+            mariazinha.RG = "13216540";
+
+            Console.WriteLine("Jogador Zequinha=> Nome: {0}", zequinha.Nome);
+            Console.WriteLine("Jogador Mariazinha=> Nome: {0}", mariazinha.Nome);
+
+            Console.ReadLine();
+        }
+    }
+}
