25f7c84845bc0a9c04193209471be0b0c35f3bd1
gitcleber <cleberhill@gmail.com>
2021-11-18 13:53:01 -0300

    - Created a project inside the 'OO' Solution, for object-oriented studies in C#.

---
+++ OO/Conceitos/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Conceitos
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Conceitos de Orientação a Objetos em C#");
+        }
+    }
+}
