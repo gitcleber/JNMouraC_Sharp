570b61ff237277c12b33c406a900cd872f337c1d
gitcleber <cleberhill@gmail.com>
2021-11-19 13:51:07 -0300

    update some lines of code

--- OO/Conceitos/Program.cs
+++ OO/Conceitos/Program.cs
@@ -1,21 +1,32 @@
 ﻿using System;
 
-namespace Conceitos
+namespace Carros
 {
     class Program
     {
         static void Main(string[] args)
         {
-            Carro monza = new Carro("monza top", 2019);
-            monza.Nome = "Monzão";
-            Console.WriteLine("Dados do monza: nome: {0}, ano: {1}, velocidade: {2}:", monza.Nome, monza.AnoFabricacao, monza.Velocidade);
+            Carro monza = new Carro("Monzão", 2019);
 
-            /*
-            Carro gol = new Carro();
-            Carro chevete = new Carro("chevete max");
-            Carro fusca = new Carro("fuscao", 1979);
-            Carro fit = new Carro(2010);            
-            */
+            Console.WriteLine($"Carro nome: {monza.Nome}\n Carro ano: {monza.AnoFabricacao}");
+
+            for (int i = 0; i < 10; i++)
+            {
+                monza.acelerar(); 
+            }
+            Console.WriteLine($"{monza.Velocidade}");
+
+            for (int i = 0; i < 5; i++)
+            {
+                monza.frear();
+            }
+            Console.WriteLine($"{monza.Velocidade}");
+
+            for (int i = 0; i < 10; i++)
+            {
+                monza.frear();
+                Console.WriteLine($"{monza.Velocidade}");
+            }           
 
             Console.ReadLine();
 
